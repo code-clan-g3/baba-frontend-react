32fb6ee2c87f4fabedc720326f9f033244d00bd5
Victor Jack <67293106+thedon-dev@users.noreply.github.com>
2024-11-18 22:37:47 +0100

    merged the aside nav

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import Desktop from "./pages/Desktop"
 function App() {
   return (
     <>
@@ -9,11 +10,11 @@ function App() {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/chatroom" element={<Desktop />} />
         </Routes>
       </BrowserRouter>
     </>
   );
 }
 
-export default App;
 export default App;

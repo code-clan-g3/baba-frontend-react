cc458bb92ff5c1d15961e788c3c717a253e579d1
Victor Jack <67293106+thedon-dev@users.noreply.github.com>
2024-11-19 22:24:40 +0100

    fixed routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import ChatRoom from "./pages/ChatRoom"
 import Register from "./pages/Register"
+import LandingPage from "./Pages/LandingPage";
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<LandingPage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Register />} />
           <Route path="/chatroom" element={<ChatRoom />} />

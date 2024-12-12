16d3ef5c65c24e54aedf19c38f995cde4bfa2707
Emmybrown775 <elumezeemma@gmail.com>
2024-12-12 15:10:58 +0100

    fixed new chat issues

--- src/components/navigation/Sidebar.jsx
+++ src/components/navigation/Sidebar.jsx
@@ -4,11 +4,44 @@ import { FaRobot, FaSpinner } from "react-icons/fa";
 import { IoSettingsOutline } from "react-icons/io5";
 
 export default function Sidebar({ isOpen, onClose, chats, isLoading }) {
+  const [newChatLoading, setNewChatLoading] = useState(false)
   const handleChatClick = (thread_id) => {
-
-    localStorage.setItem('thread_id', thread_id);
+    localStorage.setItem("thread_id", thread_id);
     window.location.reload();
   };
+
+  const handleNewChat = async () => {
+    setNewChatLoading(true);
+
+    const token =  localStorage.getItem("token")
+
+    try {
+      const response = await fetch(
+        "https://baba-python-backend.onrender.com/chat/thread",
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application.json",
+            "Authorization": `Bearer ${token}`
+          },
+        }
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+        localStorage.setItem("thread_id", data.thread_id);
+        window.location.reload();
+      } else{
+        const errorData = await response.json();
+        console.log(errorData)
+      }
+    }catch (error){
+      alert("An Error occured");
+    } finally{
+      setNewChatLoading(false);
+    }
+    
+  }
 
   return (
     <>
@@ -46,8 +79,7 @@ export default function Sidebar({ isOpen, onClose, chats, isLoading }) {
               <span className="ml-2">Loading chats...</span>
             </div>
           ) : (
-          
-          Array.isArray(chats) &&
+            Array.isArray(chats) &&
             chats.map((chat, index) => (
               <div
                 key={index}
@@ -56,8 +88,15 @@ export default function Sidebar({ isOpen, onClose, chats, isLoading }) {
               >
                 <span className="text-gray-600">{chat.chat_name}</span>
               </div>
-            )))}
+            ))
+          )}
 
+          <div
+          onClick={!newChatLoading? handleNewChat : null}
+            className={`mb-0 flex items-center space-x-3 p-2 hover:bg-gray-100 bg-indigo-600 rounded cursor-pointer ${newChatLoading ? "bg-slate-600": "bg-indigo-600"}`}
+          >
+            <span className="text-white">{!newChatLoading ? "New Chat": "Loading"}</span>
+          </div>
         </nav>
       </div>
     </>
